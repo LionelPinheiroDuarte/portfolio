ff23a2a94c9c7a624f26ca548df5cad437d28318
Liopin <contact.pinheiro.lionel@gmail.com>
2022-02-09 15:32:04 +0100

    Code highlight syntax
    	- Added the package ekeventyèplugin-syntaxhighlight
    	  that make the ability highlight the code

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
