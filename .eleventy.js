67a878d1048e818fab230b205c5f0169bd1f367a
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2024-10-08 20:28:55 +0200

    Remove useless packages and made some clean up

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,7 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const minify = require('html-minifier').minify;
 const {EleventyI18nPlugin} = require('@11ty/eleventy');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(syntaxHighlight);
-  if (process.env.NODE_ENV === 'production') {
-    config.addTransform('htmlmin', (content, path) =>
-      path.endsWith('.html')
-        ? htmlmin.minify(content, { minifyCSS: true, minifyJS: true, })
-        : content
-    )
-  }
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: 'fr'
   });
@@ -28,7 +17,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   return {
     dir: {
-      // ⚠️ These values are both relative to your input directory.
       markdownTemplateEngine: ["njk"],
       htmlTemplateEngine: "njk",
       templateFormats: ["html", "njk", "md"],
