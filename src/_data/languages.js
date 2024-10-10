2005172a0ff13519b289b91767dd98dbff64b810
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2024-10-10 18:24:33 +0200

    Add ability tochange th projects base on the page language

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -1,9 +1,11 @@
 module.exports = {
     fr: {
       dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
-      availableText: 'Cette page est disponible en:'
+      availableText: 'Cette page est disponible en:',
+      th: ['Projets', 'Description', 'Liens']
     },
     en: {
-      availableText: 'This page is also available in:'
+      availableText: 'This page is also available in:',
+      th: ['Projects', 'Description', 'Links']
     },
   };
