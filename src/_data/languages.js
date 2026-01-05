5d05add28d2dc51f7f726b893f3e50079754ab8d
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2026-01-05 15:36:55 +0100

    Add translation feature

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -1,11 +1,21 @@
 module.exports = {
     fr: {
       dir: '', // stands for the direction of the language set in the head, defaults to LTR (left to right)
-      availableText: 'Cette page est disponible en:',
-      th: ['Projet', 'Description', 'Liens'],
+	availableText: 'Cette page est disponible en:',
+	th: ['Projet', 'Description', 'Liens'],
+	section: {
+	    webdev: 'Développement Web',
+	    script: 'Scripts',
+	    automatisation: 'Automatisation'
+	}
     },
     en: {
-      availableText: 'This page is also available in:',
-      th: ['Project', 'Description', 'Links']
-    },
+	availableText: 'This page is also available in:',
+	th: ['Project', 'Description', 'Links'],
+	section: {
+	    webdev: 'Web Development',
+	    script: 'Scripts',
+	    automatisation: 'Automatisation'
+	}
+    }
   };
