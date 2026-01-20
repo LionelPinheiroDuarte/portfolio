1ab3ab08e7d29648dc91904304b9c775596a849e
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2026-01-20 14:34:44 +0100

    Remove link to brain project

--- src/_data/links.js
+++ src/_data/links.js
@@ -4,11 +4,6 @@ module.exports = {
             emoji: '📧',
             link: 'https://www.linkedin.com/in/lionel-pinheiro/',
             desc: 'Si mon profil vous interrese, contactez moi'
-        },
-        {
-            emoji: '🧠',
-            link: 'https://lionelpinheiroduarte.github.io/brain/',
-            desc: 'Où je prends mes notes'
         }
     ],
     en: [
@@ -17,10 +12,5 @@ module.exports = {
             link: 'https://www.linkedin.com/in/lionel-pinheiro/',
             desc: 'If my profile interests you, feel free to contact me.'
         },
-        {
-            emoji: '🧠',
-            link: 'https://lionelpinheiroduarte.github.io/brain/',
-            desc: 'Where I write my notes'
-        }
-    ]
+       ]
 }
